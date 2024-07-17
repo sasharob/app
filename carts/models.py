7f8de8e33ae34e0e0b4d059491674a75c554931e
SASHA_ROBL <spegoff123@yandex.ru>
2024-07-17 11:46:35 +0400

    Sessions - carts for anonymous user

--- carts/models.py
+++ carts/models.py
@@ -34,4 +34,4 @@ class Cart(models.Model):
         return round(self.product.sell_price() * self.quantity, 2)
 
     def __str__(self):
-        return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+        return f"Корзина {self.user.username if self.user else "Анонимный пользователь"} | Товар {self.product.name} | Количество {self.quantity}"
