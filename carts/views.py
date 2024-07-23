b1f9aa2e58b538251fffe59106b627a799c28a11
SASHA_ROBL <spegoff123@yandex.ru>
2024-07-23 12:31:22 +0400

    some bug fix

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
-from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-
+from django.urls import reverse
 from carts.models import Cart
 from carts.utils import get_user_carts
 from goods.models import Products
@@ -60,9 +59,17 @@ def cart_change(request):
     cart.save()
     updated_quantity = cart.quantity
 
-    cart = get_user_carts(request)
+    user_cart = get_user_carts(request)
+
+    context = {"carts": user_cart}
+
+    # if referer page is create_order add key orders: True to context
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context["orders"] = True
+
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+        "carts/includes/included_cart.html", context, request=request)
 
     response_data = {
         "message": "Количество изменено",
@@ -82,8 +89,16 @@ def cart_remove(request):
     cart.delete()
 
     user_cart = get_user_carts(request)
+
+    context = {"carts": user_cart}
+
+    # if referer page is create_order add key orders: True to context
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context["orders"] = True
+
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+        "carts/includes/included_cart.html", context, request=request)
 
     response_data = {
         "message": "Товар удалён",
